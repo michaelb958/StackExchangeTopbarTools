1c893a7cdbf53a65995eb36e7b934853d80e19d4
michaelb958 <michaelb958@riseup.net>
2013-12-13 23:10:59 +1100

    Beyond 0.1: Better floating, internal cleanup
    Damn, that was a lot of CSS hacking. On the bright side, floating is now
    presentable on every site! Not to mention that the master script itself
    can now use shorthand references to the tools object, saving many
    keystrokes.

--- StackExchangeTopbarTools.user.js
+++ StackExchangeTopbarTools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Stack Exchange Topbar Tools
 // @description A library to support all sorts of tweaks, options, and widgets for the new black topbar.
-// @version 0.1
+// @version 0.2-dev
 // @namespace michaelb958
 // @author michaelb958
 // @license MIT (http://opensource.org/licenses/MIT)
@@ -33,6 +33,86 @@ function with_jQuery(f) {
 };
 
 with_jQuery(function($) {
+  var tools = null; // set later to StackExchangeTopbarTools
+  
+  // It's sometimes handy to know what site you're on
+  //  Basically, strips leading 'meta.' and trailing anything useless
+  var sitename = (function(s) {
+    var meta = s.indexOf('meta.') === 0;
+    s = meta ? s.substring(5) : s;
+    return s.substring(0, s.indexOf('.'));
+  })(location.host);
+  
+  // While some sites can be spaced with either top margin or top
+  //  padding, some only work with one or the other.  This variable
+  //  accounts for that all-too-common case.  (Margin is used as the
+  //  default because that's the only thing that works with beta sites,
+  //  and those things are numerous as heck.)
+  var spacingAttr = (function(r, d) {
+    $.each(d, function(attr, sitestr) {
+      $.each(sitestr.split(' '), function(idx, site) {
+        r[site] = attr;
+      });
+    });
+    return r;
+  })({'': 'margin-top'}, {
+    'padding-top': 'cooking stats diy gis english wordpress cstheory apple rpg bicycles android electronics scifi skeptics drupal christianity'
+  });
+  spacingAttr = spacingAttr[sitename in spacingAttr ? sitename : ''];
+  
+  var topbar_floating_data = {
+    detect: function() { return $('.topbar').css('position') !== 'fixed'; },
+    once: $.extend(function() {
+      $('.topbar').css({'z-index': 999, 'top': 0});
+    }, {
+      askubuntu: function() {
+        $('#custom-header').css({'top': '0px', 'width': '100%'});
+        $('.topbar').css({'top': '30px', 'left': '50%', 'margin-left': '-495px'});
+      },
+      tex: function() {
+        $('body').css('background-image', 'none');
+      }
+    }),
+    on: $.extend(function() {
+      $('.topbar').css('position', 'fixed');
+      $('.container').css(spacingAttr, '34px');
+    }, {
+      askubuntu: function() {
+        $('#custom-header').css('position', 'fixed');
+        $('.topbar').css('margin-left', '-495px');
+        $('.container').css(spacingAttr, '62px');
+      },
+    }),
+    off: $.extend(function() {
+      $('.topbar').css('position', 'static');
+      $('.container').css(spacingAttr, 0);
+    }, {
+      askubuntu: function() {
+        $('#custom-header').css('position', 'static');
+        $('.topbar').css('margin-left', 'auto');
+      },
+    })
+  };
+  
+  var topbar_fullWidth_data = {
+    detect: function() { return $('.topbar *.topbar-wrapper').css('width') === '980px'; },
+    on: $.extend(function() {
+      $('.topbar *.topbar-wrapper').css('width', '100%');
+      $('.topbar *.search-container *:last-child').css('margin-right', '2px');
+    }, {
+      askubuntu: function() {
+        // Not supported for aesthetic reasons
+        tools.topbar.fullWidth(false);
+      },
+    }),
+    off: $.extend(function() {
+      $('.topbar *.topbar-wrapper').css('width', '980px');
+      $('.topbar *.search-container *:last-child').css('margin-right', '0');
+    }, {})
+  };
+  
+  // ----------------------------------------------------------------------------------------------
+  
   $.fn.isAttached = function() {
     // Credit @SLaks <http://stackoverflow.com/a/3086084/1053021>
     return $.contains(document.documentElement, this[0]);
@@ -41,27 +121,25 @@ with_jQuery(function($) {
   window.StackExchangeTopbarTools = {
     _tick_subscribers: [],
     
+    sitename: sitename,
+    
     topbar: {
       floating: function(toggle) {
-        if ((typeof toggle === 'undefined' && $('.topbar').css('position') !== 'fixed')
-            || toggle) {
-          $('.topbar').css('position', 'fixed');
-          $('.container').css('margin-top', '34px');
-        } else {
-          $('.topbar').css('position', 'static');
-          $('.container').css('margin-top', '0');
-        }
+        tools.topbar._styleToggle.call(topbar_floating_data, toggle);
       },
-      
       fullWidth: function(toggle) {
-        if ((typeof toggle === 'undefined' && $('.topbar *.topbar-wrapper').css('width') === '980px')
-            || toggle) {
-          $('.topbar *.topbar-wrapper').css('width', '100%');
-          $('.topbar *.search-container *:last-child').css('margin-right', '4px');
-        } else {
-          $('.topbar *.topbar-wrapper').css('width', '980px');
-          $('.topbar *.search-container *:last-child').css('margin-right', '0');
+        tools.topbar._styleToggle.call(topbar_fullWidth_data, toggle);
+      },
+      _styleToggle: function(toggle) {
+        var x; // temp var
+        if (typeof toggle === 'undefined') toggle = this.detect();
+        if (toggle && (x = this.once)) {
+          x();
+          (x = x[sitename in x ? sitename : '']) ? x() : null;
+          this.once = false;
         }
+        (x = toggle ? this.on : this.off)();
+        (x = x[sitename in x ? sitename : '']) ? x() : null;
       },
       
       color: function(color) {
@@ -75,10 +153,10 @@ with_jQuery(function($) {
       },
       
       append: function() {
-        StackExchangeTopbarTools.links._add.apply({prepend: false}, arguments);
+        tools.links._add.apply({prepend: false}, arguments);
       },
       prepend: function() {
-        StackExchangeTopbarTools.links._add.apply({prepend: true}, arguments);
+        tools.links._add.apply({prepend: true}, arguments);
       },
       _add: function() {
         var _ = this;
@@ -113,7 +191,7 @@ with_jQuery(function($) {
           
           if (data.on) {
             if (data.on.tick) {
-              StackExchangeTopbarTools._tick_subscribers.push({
+              tools._tick_subscribers.push({
                 func: data.on.tick,
                 elem: elem,
                 interval: 'short',
@@ -121,8 +199,8 @@ with_jQuery(function($) {
             }
           }
           
-          if (StackExchangeTopbarTools.links._defaults.color) {
-            elem.css('color', StackExchangeTopbarTools.links._defaults.color);
+          if (tools.links._defaults.color) {
+            elem.css('color', tools.links._defaults.color);
           }
           
           elem[_.prepend ? 'prependTo' : 'appendTo']('.topbar-menu-links');
@@ -140,7 +218,7 @@ with_jQuery(function($) {
       
       color: function(color) {
         $('.topbar-menu-links a, .topbar-flair span').css('color', color);
-        StackExchangeTopbarTools.links._defaults.color = color;
+        tools.links._defaults.color = color;
       },
     }, // links
     
@@ -152,7 +230,7 @@ with_jQuery(function($) {
         initFuncs = StackExchangeTopbarToolsPluginInit;
       }
       while (initFuncs.length) {
-        initFuncs.shift()(StackExchangeTopbarTools);
+        initFuncs.shift()(tools);
       }
     },
   };
@@ -160,18 +238,17 @@ with_jQuery(function($) {
   setInterval(function() {
     // The `* 1000` is to correct for `.getTime()` being in milliseconds and the offset in seconds
     var t = new Date().getTime() + StackExchange.options.serverTimeOffsetSec * 1000;
-    $.each(StackExchangeTopbarTools._tick_subscribers, function() {
+    $.each(tools._tick_subscribers, function() {
       if (!(this.elem.isAttached())) return true; // link not attached to DOM; do nothing
       if (this.interval === 'short')
         this.func.call($.extend({}, this), new Date(t));
     });
   }, 1000);
   
-  $('.topbar').css({'z-index': 999, 'top': 0});
   $('.topbar *.network-items *.topbar-icon').removeAttr('href');
   $('.topbar *.profile-me').css({'margin-right': '5px', 'padding-right': '0'});
   $('.topbar *.topbar-menu-links').css('margin-left', '0');
   $('.topbar *.search-container *:last-child').css('margin-left', '0');
   
-  StackExchangeTopbarTools.pluginsReady();
+  (tools = window.StackExchangeTopbarTools).pluginsReady();
 });
